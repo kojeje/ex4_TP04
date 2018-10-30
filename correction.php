3c0b6f5b892130a93610b1a31ec463b0e18cd779
kojeje <kojeje.dev@gmail.com>
2018-10-30 15:01:53 +0100

    dev

--- correction.php
+++ correction.php
@@ -7,11 +7,10 @@
      */
 
 
-
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $fails= [];
+    $fails = [];
     if (empty($email)) {
         $fails[] = "email";
     }
@@ -21,47 +20,47 @@
     }
 
     if (count($fails) > 0) {
-        echo "Les(s) champ(s) ";
-        foreach ($fails as $fails) {
-            echo $fails . " ";
+    echo "Les(s) champ(s) ";
+    foreach ($fails as $fails) {
+        echo $fails . " ";
 
-        }
-        echo "est/sont vide(s).";
-    } else {
+    }
+    echo "est/sont vide(s).";
+} else {
 
-        $connexionBdd = mysqli_connect("localhost", "root", "root");
-        mysqli_set_charset($connexionBdd, "utf8");
-        $selectionBase = mysqli_select_db($connexionBdd, "test");
+    $connexionBdd = mysqli_connect("localhost", "root", "root");
+    mysqli_set_charset($connexionBdd, "utf8");
+    $selectionBase = mysqli_select_db($connexionBdd, "test");
 
-        $requete = "SELECT firstname, lastname 
+    $requete = "SELECT firstname, lastname 
         FROM user_list 
         WHERE email =  \"$email\" 
         AND password = \"$password\"
         LIMIT 1";
 
-        $query = mysqli_query($connexionBdd = $requete);
-        $result = mysqli_fetch_assoc($query);
+    $query = mysqli_query($connexionBdd = $requete);
+    $result = mysqli_fetch_assoc($query);
 
-        if (is_array($result)) {
-            echo "Bonjour" . $result['firstname'] . " " . $result ['last_name'];
-        } else {
+    if (is_array($result)) {
+        echo "Bonjour" . $result['firstname'] . " " . $result ['last_name'];
+    } else {
 
-            $requete_email = "SELECT COUNT (*) 
+        $requete_email = "SELECT COUNT (*) 
             FROM user_list 
             WHERE email = \"$email\";";
-            $query_email = mysqli_query($connexionBdd, $requete_email);
-            $result_email = mysqli_fetch_assoc($query_email);
-            if ($result_email[0] == "0") {
-                echo "le mot de passe n'est pas correctement écrit"
+        $query_email = mysqli_query($connexionBdd, $requete_email);
+        $result_email = mysqli_fetch_assoc($query_email);
+        if ($result_email[0] == "0") {
+            echo "le mot de passe n'est pas correctement écrit"
             } else {
-                echo "L'email n'a pas été trouvé ... Veuillez créer votre compte...";
-            }
-            echo ($result_email[0] == "1")
-                ? "le mot de passe n'est pas correctement écrit"
-                : "L'email n'a pas été trouvé ... Veuillez créer votre compte...";
-
-
+            echo "L'email n'a pas été trouvé ... Veuillez créer votre compte...";
         }
+        echo ($result_email[0] == "1")
+            ? "le mot de passe n'est pas correctement écrit"
+            : "L'email n'a pas été trouvé ... Veuillez créer votre compte...";
 
 
-    ?>
+    }
+
+
+?>
